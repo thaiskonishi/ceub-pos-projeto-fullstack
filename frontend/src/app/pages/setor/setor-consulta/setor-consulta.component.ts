b733e8acd1463a1b32302ae134d3f5a29972f152
ThaisMK <thais.konishi@gmail.com>
2024-08-27 21:09:12 -0300

    ajuste componente setor consulta e css de login

--- frontend/src/app/pages/setor/setor-consulta/setor-consulta.component.ts
+++ frontend/src/app/pages/setor/setor-consulta/setor-consulta.component.ts
@@ -2,15 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { SetorDto, SetorService } from '../../../services/setor.service';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
 	selector: 'app-setor-consulta',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule,MatButtonModule,MatTableModule],
 	templateUrl: './setor-consulta.component.html',
 	styleUrl: './setor-consulta.component.scss',
 })
 export class SetorConsultaComponent implements OnInit {
+	displayedColumns: string[] = ['ID', 'NOME DO SETOR', 'DESCRIÇÃO', 'AÇÕES'];
 	setores: SetorDto[] = [];
 
 	constructor(private setorService: SetorService, private router: Router) {}
